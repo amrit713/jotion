41ea3e4ec8aacedab8c6f78a40b0c02d5f249d15
amrit713 <amritghimire741443@gmail.com>
2023-11-04 10:11:59 +0545

    notion langing page

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,10 +1,17 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { Heading } from "./_components/heading";
+import { Heroes } from "./_components/heroes";
+import { Footer } from "./_components/footer";
 
-export default function Home() {
+export default function MarketingPage() {
   return (
-    <div className="font-semibold text-red-900">
-      <Button variant="ghost">Hellos</Button>
+    <div className="min-h-full flex flex-col dark:bg-[#1f1f1f]">
+      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+        <Heading />
+        <Heroes />
+        <Footer />
+      </div>
     </div>
   );
 }
